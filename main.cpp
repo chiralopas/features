a524c1c1721962578fb135585a758c4be870e563
Vishal <chiral.opas@gmail.com>
2025-04-18 20:06:15 +0530

    provided appropriate value for attribute

--- main.cpp
+++ main.cpp
@@ -22,7 +22,7 @@ void initialize()
     glBindBuffer(GL_ARRAY_BUFFER, Buffers[0]);
     glBufferData(GL_ARRAY_BUFFER, sizeof(triangle1), triangle1, GL_STATIC_DRAW);
 
-    glVertexAttribPointer(0, 2, GL_FLOAT, GL_FALSE, 0, (void*)0);
+    glVertexAttribPointer(0, 2, GL_FLOAT, GL_FALSE, 2*sizeof(float), (void*)0);
     glEnableVertexAttribArray(0);
 
     ShaderInfo shaders[] =
