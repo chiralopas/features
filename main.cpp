55b149a26a50a1aad15b87ef320a4b0c235ea458
Vishal <chiral.opas@gmail.com>
2025-04-29 09:48:33 +0530

    multiple shaders using one uniform block

--- main.cpp
+++ main.cpp
@@ -8,11 +8,16 @@
 GLuint VAOs[1];
 GLuint Buffers[1];
 
-// initialize the data
+GLuint program1;
+GLuint program2;
+
+//  initialize the data
 void initialize()
 {
-    GLfloat triangle1[3][2] = {
-        {-0.5f,-0.5f}, { 0.0f, 0.5f}, { 0.5f,-0.5f}
+
+    GLfloat triangles[6][2] = {
+        { -0.90f, -0.90f }, {  0.85f, -0.90f }, { -0.90f,  0.85f },
+        {  0.90f, -0.85f }, {  0.90f,  0.90f }, { -0.85f,  0.90f }
     };
 
     glGenVertexArrays(1, VAOs);
@@ -21,32 +26,40 @@ void initialize()
     glBindVertexArray(VAOs[0]);
 
     glBindBuffer(GL_ARRAY_BUFFER, Buffers[0]);
-    glBufferData(GL_ARRAY_BUFFER, sizeof(triangle1), triangle1, GL_STATIC_DRAW);
+    glBufferData(GL_ARRAY_BUFFER, sizeof(triangles), triangles, GL_STATIC_DRAW);
 
     glVertexAttribPointer(0, 2, GL_FLOAT, GL_FALSE, 2*sizeof(float), (void*)0);
     glEnableVertexAttribArray(0);
 
-    ShaderInfo shaders[] =
+    ShaderInfo shaders1[] =
     {
-        {GL_VERTEX_SHADER, "../triangle.vert"},
-        {GL_FRAGMENT_SHADER, "../triangle.frag"},
-        {GL_NONE, NULL }
+        { GL_VERTEX_SHADER, "../triangle.vert" },
+        { GL_FRAGMENT_SHADER, "../triangle.frag" },
+        { GL_NONE, NULL }
     };
 
-    GLuint program = LoadShaders(shaders);
-    glUseProgram(program);
+    program1 = LoadShaders(shaders1);
+
+    ShaderInfo shaders2[] =
+    {
+        { GL_VERTEX_SHADER, "../triangle.vert" },
+        { GL_FRAGMENT_SHADER, "../orange.frag" },
+        { GL_NONE, NULL }
+    };
+
+    program2 = LoadShaders(shaders2);
 
     // uniform buffer object //
-    
+
     /* Initialize uniform values in uniform block "Uniforms" */
     GLint uboSize;
     GLuint ubo;
     GLvoid* buffer;
 
     /* Find the uniform buffer index for "Uniforms", and determine the block’s sizes */
-    GLuint uboIndex = glGetUniformBlockIndex(program,"Transformation");
-    glGetActiveUniformBlockiv(program, uboIndex, GL_UNIFORM_BLOCK_DATA_SIZE, &uboSize);
-    
+    GLuint uboIndex = glGetUniformBlockIndex(program1, "Transformation");
+    glGetActiveUniformBlockiv(program1, uboIndex, GL_UNIFORM_BLOCK_DATA_SIZE, &uboSize);
+
     buffer = malloc(uboSize);
 
     if (buffer == NULL) {
@@ -56,7 +69,7 @@ void initialize()
     else
     {
         enum { Translation, Rotation, Scale, NumUniforms };
-        
+
         /* values to be stored in the buffer object */
         GLfloat translation[] = { 0.0, 0.5, 0.0 };
         GLfloat rotation[] = { 90.0, 0.0, 0.0, 1.0 };
@@ -74,16 +87,17 @@ void initialize()
         GLint offset[NumUniforms];
         GLint type[NumUniforms];
 
-        glGetUniformIndices(program, NumUniforms, names, indices);
-        glGetActiveUniformsiv(program, NumUniforms, indices, GL_UNIFORM_OFFSET, offset);
-        glGetActiveUniformsiv(program, NumUniforms, indices, GL_UNIFORM_SIZE, size);
-        glGetActiveUniformsiv(program, NumUniforms, indices, GL_UNIFORM_TYPE, type);
+        /* we need program1 just to calculate offset, size and type of uniform block*/
+        glGetUniformIndices(program1, NumUniforms, names, indices);
+        glGetActiveUniformsiv(program1, NumUniforms, indices, GL_UNIFORM_OFFSET, offset);
+        glGetActiveUniformsiv(program1, NumUniforms, indices, GL_UNIFORM_SIZE, size);
+        glGetActiveUniformsiv(program1, NumUniforms, indices, GL_UNIFORM_TYPE, type);
 
         /* copy the uniform values into the buffer */
         memcpy((std::byte*)buffer + offset[Translation], &translation, size[Translation] * 3*sizeof(GLfloat));
         memcpy((std::byte*)buffer + offset[Rotation], &rotation, size[Rotation] * 4*sizeof(GLfloat));
         memcpy((std::byte*)buffer + offset[Scale], &scale, size[Scale] * 1*sizeof(GLfloat));
- 
+
         /* create the uniform buffer object, initialize its storage, and associated it with the shader program */
         glGenBuffers(1, &ubo);
         glBindBuffer(GL_UNIFORM_BUFFER, ubo);
@@ -98,7 +112,12 @@ void render()
     glClear(GL_COLOR_BUFFER_BIT);
 
     glBindVertexArray(VAOs[0]);
+
+    glUseProgram(program1);
     glDrawArrays(GL_TRIANGLES, 0, 3);
+
+    glUseProgram(program2);
+    glDrawArrays(GL_TRIANGLES, 3, 6);
 }
 
 int main()
