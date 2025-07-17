d7977478348930cb323f6c77d2bd4a3b97e202a6
Vishal <chiral.opas@gmail.com>
2025-07-17 22:06:25 +0530

    animated two frames for a triangle

--- main.cpp
+++ main.cpp
@@ -2,16 +2,25 @@
 #include <GLFW/glfw3.h>
 #include <iostream>
 #include "LoadShaders.h"
+#include <thread>
 
 
 GLuint VAOs[1];
 GLuint Buffers[1];
+GLuint EBOs[1];
+bool flag = true;
 
 // initialize the data
 void initialize()
 {
-    GLfloat triangle1[3][2] = {
-        {-0.5f,-0.5f}, { 0.0f, 0.5f}, { 0.5f,-0.5f}
+
+    GLfloat triangles[] = {
+        -0.5f, -0.5f, 0.0f, 0.5f, 0.5f, -0.5f,
+        -0.5f, -0.5f, 0.0f, 0.5f, 0.5f, -0.8f
+    };
+
+    GLushort indices[] = {
+        0, 1, 2
     };
 
     glGenVertexArrays(1, VAOs);
@@ -20,10 +29,15 @@ void initialize()
     glBindVertexArray(VAOs[0]);
 
     glBindBuffer(GL_ARRAY_BUFFER, Buffers[0]);
-    glBufferData(GL_ARRAY_BUFFER, sizeof(triangle1), triangle1, GL_STATIC_DRAW);
+    glBufferData(GL_ARRAY_BUFFER, sizeof(triangles), triangles, GL_STATIC_DRAW);
 
     glVertexAttribPointer(0, 2, GL_FLOAT, GL_FALSE, 2*sizeof(float), (void*)0);
     glEnableVertexAttribArray(0);
+
+    // element buffer setup
+    glGenBuffers(1, EBOs);
+    glBindBuffer(GL_ELEMENT_ARRAY_BUFFER, EBOs[0]);
+    glBufferData(GL_ELEMENT_ARRAY_BUFFER, sizeof(indices), indices, GL_STATIC_DRAW);
 
     ShaderInfo shaders[] =
     {
@@ -40,9 +54,18 @@ void initialize()
 void render()
 {
     glClear(GL_COLOR_BUFFER_BIT);
-
     glBindVertexArray(VAOs[0]);
-    glDrawArrays(GL_TRIANGLES, 0, 3);
+
+    if (flag)
+        glDrawElementsBaseVertex(GL_TRIANGLES, 3, GL_UNSIGNED_SHORT, NULL, 0);
+    else    
+        glDrawElementsBaseVertex(GL_TRIANGLES, 3, GL_UNSIGNED_SHORT, NULL, 3);
+
+    flag = !flag;
+
+    /* this halts the entire application for given time,
+       but good enough for completing example code */
+    std::this_thread::sleep_for(std::chrono::milliseconds(100));
 }
 
 int main()
